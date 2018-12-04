9e4724989ad8068df6dc7cb8cea3b30942d2f8c2
Kimjunyoung <wellkiim_@Kimjunyoungui-MacBook-Pro.local>
2018-12-05 00:05:22 +0900

    Creating React Components with JSX

--- src/Movie.js
+++ src/Movie.js
@@ -15,7 +15,7 @@ class Movie extends Component {
 class MoviePoster extends Component{
     render(){
         return(
-            <img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiT042S3YHfAhWCPXAKHeJKBcIQjRx6BAgBEAU&url=http%3A%2F%2Fharrypotter.wikia.com%2Fwiki%2FFantastic_Beasts%3A_The_Crimes_of_Grindelwald&psig=AOvVaw0BQ4MCNv5vDYt0uTTTgz3b&ust=1543860084754878" />
+            <img src="" />
         )
     }
 }
