708c5609dd4a576218002008c71228c333e938be
fred <fredkeemhaus>
2018-12-12 00:45:45 +0900

    Ajax in React

--- src/App.js
+++ src/App.js
@@ -5,37 +5,11 @@ import Movie from './Movie';
 class App extends Component {
   // Render : componentWillMount() -> render() -> componentDidMount()
   // Update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> component
-  state = {
-    
-  }
+
+  state = {}
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          {
-            title: "Grindelwald Crimes",
-            poster: "http://www.gstatic.com/tv/thumb/movieposters/13663846/p13663846_p_v8_ag.jpg"
-          },
-          {
-            title: "Ready Player One",
-            poster: "http://www.gstatic.com/tv/thumb/v22vodart/12806300/p12806300_v_v8_ac.jpg"
-          },
-          {
-            title: "Bohemian Rhapsody",
-            poster: "http://www.gstatic.com/tv/thumb/v22vodart/15444050/p15444050_v_v8_as.jpg"
-          },
-          {
-            title: "Upgrade",
-            poster: "https://static1.squarespace.com/static/511eea22e4b06642027a9a99/t/5b760d2b898583cd4bf82f6a/1534463280060/Upgrade.jpg"
-          },
-          {
-            title: "Aqua Man",
-            poster: "http://img09.deviantart.net/3d97/i/2016/130/f/a/aquaman_2016_movie_poster_by_edaba7-d9fhens.jpg"
-          },
-        ]
-      })
-    }, 5000)
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
   }
 
   _renderMovies = () => {
